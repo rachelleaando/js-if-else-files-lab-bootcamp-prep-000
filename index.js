172dda470f53ea53efe0d9fb963a7ce1228cc819
Rachelle Ando <rachellelorraineandoart@gmail.com>
2018-11-18 16:04:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-var index = require("./tests.js")
-
-if (index.name === "Susan") {
-    console.log("The name is correct")
-} else {
-    console.log("Expected: Susan, Received: "+index.name)
-}
-
-
-
-if (index.height === 70) {
-    console.log("The height is correct")
-} else {
-    console.log("Expected: 70, Received: "+index.height)
+var name = "Joe"
+var height = "74"
+ 
+// Don't worry about this
+module.exports = { name, height
 }

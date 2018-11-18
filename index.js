a5ad67d73a849afaa77fa5b9f00d408375449e95
Rachelle Ando <rachellelorraineandoart@gmail.com>
2018-11-18 15:34:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
-if (name === "Susan") {
+if (test.js === "Susan") {
     console.log("The name is correct")
 }
 else {

cd09583f54fe0470650959048de0b6209c71c8f8
Rachelle Ando <rachellelorraineandoart@gmail.com>
2018-11-18 16:27:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var index = require("./tests.js")
+var name = "Susan"
+var height = 70
+ 
+// Don't worry about this
+module.exports = { name, height
+}
 
 if (index.name === "Susan") {
     console.log("The name is correct")
